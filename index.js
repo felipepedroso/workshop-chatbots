66a3b1e59d34e8ea75bb8076311ee61d36003a20
Felipe Pedroso <f.pedroso677@gmail.com>
2017-09-02 21:12:35 -0300

    First step: creating an echo bot.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello bots!");
+var restify = require('restify');
+var builder = require('botbuilder');
+
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+server.post('/api/messages', connector.listen());
+
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("Message Received: %s", session.message.text);
+});
